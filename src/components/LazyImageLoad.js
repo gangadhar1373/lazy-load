2e583b7a805e2db18da136018b7081636ae3792d
Gangadhar Mullapudi <gangadhar1373@gmail.com>
2021-01-22 15:56:36 -0600

    updated css and added prop types

--- src/components/LazyImageLoad.js
+++ src/components/LazyImageLoad.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 import './LazyImageLoad.css'
+import { string } from 'prop-types'
 
 const LazyImageLoad = (props) => {
     const { thumbnail, src } = props
     const [isLoaded, setIsLoaded] = useState(false)
     return (
         <div className='lazy-load-image-wrapper'>
-            <img className='image' src={thumbnail} alt='nike'
-                style={{ visibility: isLoaded ? "hidden" : "visible" }}
+            <img className='image thumb' src={thumbnail} alt='nike'
+                style={{ visibility: isLoaded ? 'hidden' : 'visible' }}
             />
             <img
                 onLoad={() => {
                     setIsLoaded(true);
                 }}
-                className='image'
+                className='image full'
                 style={{ opacity: isLoaded ? 1 : 0 }}
                 alt='nike'
                 src={src}
@@ -21,5 +22,15 @@ const LazyImageLoad = (props) => {
         </div>
     )
 }
+
+LazyImageLoad.propTypes = {
+    thumbnail: string,
+    src: string,
+  };
+  
+  LazyImageLoad.defaultProps = {
+    thumbnail: null,
+    src: null,
+  };
 
 export default LazyImageLoad;
